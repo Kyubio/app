023563dab6660b6d54f9f899333394c198425adc
Cheyenne Bolders <c.bolders@student.fontys.nl>
2021-05-11 11:22:29 +0200

    local storage toegevoegd om gegevens van sessies toe te voegen.

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,26 +1,40 @@
+var CurrentSessionID;
+var SessionID = localStorage.getItem("CurrentSessionID");
+if(SessionID == null)
+{
+  localStorage.setItem("CurrentSessionID", 1);
+}
+
 var d = new Date;
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var month = months[d.getMonth()];
 var logDate = d.getDate() + " " +  month;
 
-var Progress = "75%";
+var Progress;
 
 var setTime;
 
 var frequency = 5;
 
+var lastSessionLog;
+
 function CreateButton()
 {
-  var lastSessionLog = "<button class='SessionLog'> <div> <div id='progression'>" + Progress + "</div> <div> <p>" + logDate + "</p> <p>" + setTime + "</p> </div> <i class='far fa-chevron-right'></i></div> </button>";
+  var curSession = localStorage.getItem("CurrentSessionID");
+  setTime = localStorage.getItem("time"+curSession);
+  Progress = localStorage.getItem("progress"+curSession);
 
-  $("#earlierSessionLog").append(lastSessionLog);
+  lastSessionLog = "<button class='SessionLog'> <div> <div id='progression'>" + Progress + "</div> <div> <p>" + logDate + "</p> <p>" + setTime + "</p> </div> <i class='far fa-chevron-right'></i></div> </button>";
+  localStorage.setItem("button"+curSession, lastSessionLog);
+  
 }
 
 //check welke tijd er wordt ingesteld en zet deze in de var setTime
 $("#timerCnfrm").click(function(){
+  Progress = "0%";
   setTime = $("#timeSelect").val();
-  localStorage.setItem("time", setTime);
-  console.log(setTime);
+  localStorage.setItem("time"+SessionID, setTime);
+  localStorage.setItem("progress"+SessionID, Progress);
 });
 
 //lets start knop op de session settings pagina listener
@@ -31,7 +45,13 @@ $("#settingsCnfrm").click(function(){
     msgTrilMotor = new Paho.MQTT.Message("1");
     msgTrilMotor.destinationName = "trilMotor";
     mqtt.send(msgTrilMotor);
-    console.log(msgTrilMotor)
+    console.log(msgTrilMotor);
+
+    CreateButton();
+    var NubSession = parseFloat(SessionID);
+    NubSession = NubSession + 1;
+    localStorage.setItem("CurrentSessionID", NubSession);
+
     
 });
 
@@ -56,5 +76,7 @@ function setBubble(range, bubble) {
 
   // Sorta magic numbers based on size of the native UI thumb
   bubble.style.left = `calc(${newVal}% + (${8- newVal * 0.15}px))`;
+
+  localStorage.setItem("frequency"+SessionID, val);
 }
 
