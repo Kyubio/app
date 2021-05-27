f2e973a59acbaa136c8128717a431433dea7c3b5
Cheyenne Bolders <c.bolders@student.fontys.nl>
2021-05-27 15:31:59 +0200

    ervoor gezorgd dat je geen timer van 0 kunt instellen

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -47,7 +47,7 @@ function CreateButton() {
 }
 
 //check welke tijd er wordt ingesteld en zet deze in de var setTime
-$("#timerCnfrm").click(function () {
+$("#timerCnfrm").click(function () { 
     Progress = "0";
     setTime = $("#timeSelect").val();
 
@@ -75,6 +75,11 @@ $("#timerCnfrm").click(function () {
     if(timeMili == 0)
     {
         localStorage.setItem("progress"+ SessionID, 100);
+        toggleFrequencyShow();   
+    }
+    else
+    {
+        window.location.href = "sessionSettings.html";
     }
 });
 
@@ -117,3 +122,50 @@ function setBubble(range, bubble) {
 
     localStorage.setItem("frequency" + SessionID, val); //localStorage
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function showChangeNameModule() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+//modal frequency
+var info = document.getElementById("nope");
+
+function toggleFrequencyShow() {
+    info.style.display = "block";
+}
+
+function toggleFrequencyHide() {
+    info.style.display = "none";
+}
+
+span.onclick = function () {
+    info.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == info) {
+        info.style.display = "none";
+    }
+};
